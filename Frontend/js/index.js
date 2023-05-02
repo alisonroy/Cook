b628aecac950cda89163844788dbd427f6dda849
alisonroy <alisonroy12@gmail.com>
2023-05-02 08:45:13 +0530

    test

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -10,6 +10,22 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 document.getElementById("snap").addEventListener("click", () => {
   context.drawImage(video, 0, 0, 250, 200);
 });
+console.log("tes");
+$("body").on("click", ".tts", function () {
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = "en";
+  speech.text = $(this).val();
+  window.speechSynthesis.speak(speech);
+});
+$("body").on("click", ".pause", function () {
+  window.speechSynthesis.pause();
+});
+$("body").on("click", ".resume", function () {
+  window.speechSynthesis.resume();
+});
+$("body").on("click", ".cancel", function () {
+  window.speechSynthesis.cancel();
+});
 
 document.getElementById("getrec").addEventListener("click", (event) => {
   event.preventDefault();
@@ -43,10 +59,43 @@ document.getElementById("getrec").addEventListener("click", (event) => {
           response[i][1] +
           "<br> Recipe: " +
           response[i][4];
+        var b1 = document.createElement("button");
+        b1.type = "button";
+        b1.classList.add("tts");
+        b1.value =
+          "Cuisine:" +
+          response[i][2] +
+          "Ingredients:" +
+          response[i][3] +
+          "Cook Time:" +
+          response[i][1] +
+          "Recipe: " +
+          response[i][4];
+        b1txt = document.createTextNode("Read it Out!!!");
+        b1.append(b1txt);
+        var b2 = document.createElement("button");
+        b2.type = "button";
+        b2.classList.add("ttsb", "resume");
+        b2txt = document.createTextNode("Resume");
+        b2.append(b2txt);
+        var b3 = document.createElement("button");
+        b3.type = "button";
+        b3.classList.add("ttsb", "pause");
+        b3txt = document.createTextNode("Pause");
+        b3.append(b3txt);
+        var b4 = document.createElement("button");
+        b4.type = "button";
+        b4.classList.add("ttsb", "cancel");
+        b4txt = document.createTextNode("Cancel");
+        b4.append(b4txt);
 
         h1.appendChild(h1txt);
         divin.appendChild(h1);
         divin.appendChild(p);
+        divin.appendChild(b1);
+        divin.appendChild(b2);
+        divin.appendChild(b3);
+        divin.appendChild(b4);
         div.appendChild(divin);
         area.appendChild(div);
       }
